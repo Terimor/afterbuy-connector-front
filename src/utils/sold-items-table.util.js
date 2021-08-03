e6306780eeafcbeb91c968346d9c12789fb2fdf0
Vladyslav <vkyrychenko@aer.de>
2021-08-03 17:08:19 +0300

    added extra fields in sold items table

--- src/utils/sold-items-table.util.js
+++ src/utils/sold-items-table.util.js
@@ -5,12 +5,17 @@ export const columns = [
     {id: 'price', label: 'Price'},
     {id: 'volume', label: 'Volume'},
     {id: 'product_code', label: 'Product code'},
+    {id: 'afterbuy_account', label: 'Afterbuy account'},
+    {id: 'date_time', label: 'Date'},
 ];
 
 export const flattenResponse = (rows) => {
     return rows.map((row) => {
         return {
-            ...row, volume: row.volume ? `${row.volume.amount} ${row.volume.unit}` : '-'
+            ...row,
+            volume: row.volume ? `${row.volume.amount} ${row.volume.unit}` : '-',
+            afterbuy_account: row.order.afterbuy_account_name,
+            date_time: row.order.date_time
         }
     })
 }
